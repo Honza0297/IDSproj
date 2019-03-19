dd1792f47cb1a23639d85cef5a787dbab57ff2da
denny101603 <43618398+denny101603@users.noreply.github.com>
2019-03-19 19:53:57 +0100

    pridano naplneni tabulek ukazkovymi daty

--- IDSproj.sql
+++ IDSproj.sql
@@ -72,7 +72,7 @@ CREATE TABLE Leky(
 
 CREATE TABLE Predpisy( 
     kod_predpisu NUMBER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-    davkovani VARCHAR(20) NOT NULL,
+    davkovani VARCHAR(255) NOT NULL,
     doba_podavani VARCHAR(20),    
     podan_v_ordinaci VARCHAR(3) NOT NULL,
     CONSTRAINT podan_v_ordinaci CHECK (podan_v_ordinaci IN ('ANO', 'NE')),
@@ -101,7 +101,7 @@ CREATE TABLE Urceni_leku_pro_nemoc (
 );
 
 CREATE TABLE Davkovani_pro_druh(
-    doporucene_davkovani number not null,
+    doporucene_davkovani VARCHAR(255) not null,
     kod_leku NUMBER NOT NULL,
     CONSTRAINT da_kod_leku FOREIGN KEY (kod_leku) REFERENCES Leky(kod_leku),
     
@@ -111,12 +111,120 @@ CREATE TABLE Davkovani_pro_druh(
 
 
 INSERT INTO Osoby(jmeno, prijmeni, ulice, cislo_popisne, mesto, psc) 
-    VALUES('Jan', 'Beran', 'Bozetechova', 2, 'Ceska Trebova', 56003);
+    VALUES('Jan', 'Beran', 'Božetěchova', 2, 'Česká Třebová', 56003);
+
+INSERT INTO Osoby(jmeno, prijmeni, titul, ulice, cislo_popisne, mesto, psc) 
+    VALUES('Stanislav', 'Stejskal','MVDr.', 'Kuldova', 654, 'Brno', 60025);
+
+INSERT INTO Osoby(jmeno, prijmeni, ulice, cislo_popisne, mesto, psc) 
+    VALUES('Josef', 'Vysokomýtský', 'Ústecká', 1002, 'Liberec', 56203);
+
+INSERT INTO Osoby(jmeno, prijmeni, titul, ulice, cislo_popisne, mesto, psc) 
+    VALUES('Jasan', 'Statham', 'Ph.D. Ing.','1st Avenue', 65487, 'New York', 56991);
+
+INSERT INTO Osoby(jmeno, prijmeni, ulice, cislo_popisne, mesto, psc) 
+    VALUES('Kamil', 'Vondra', 'Ostravska', 21, 'Opava', 96003);
+
+INSERT INTO Osoby(jmeno, prijmeni, ulice, cislo_popisne, mesto, psc) 
+    VALUES('Kamila', 'Krátká', 'Bří Čapků', 201, 'Aš', 16003);
+
+INSERT INTO Zamestnanci(rc, cislo_uctu, kod_banky, pozice, hodinova_mzda, osobni_zaznamy) 
+    VALUES(0405805487, 1236548942, 300, 'doktor', 220, (SELECT osobni_cislo FROM Osoby WHERE prijmeni='Stejskal' AND jmeno='Stanislav'));
+
+INSERT INTO Zamestnanci(rc, cislo_uctu, kod_banky, pozice, hodinova_mzda, osobni_zaznamy) 
+    VALUES(0405905487, 1236543242, 3030, 'sestra', 175, (SELECT osobni_cislo FROM Osoby WHERE prijmeni='Krátká' AND jmeno='Kamila'));
+
     
 INSERT INTO Druhy(nazev)
-    VALUES('kocka');
-    
-INSERT INTO Zvirata(jmeno, datum_narozeni, datum_posledni_prohlidky, vlastnik, druh) 
-    VALUES('Micka', DATE'1998-12-20', DATE'2019-3-19', (SELECT osobni_cislo FROM Osoby WHERE jmeno='Jan' AND prijmeni='Beran' ), (SELECT id_druhu FROM Druhy WHERE nazev='kocka' ) );
+    VALUES('kočka');
 
+INSERT INTO Druhy(nazev)
+    VALUES('pes');
+
+INSERT INTO Druhy(nazev)
+    VALUES('krokodýl');
+
+INSERT INTO Druhy(nazev)
+    VALUES('potkan');
+
+INSERT INTO Druhy(nazev)
+    VALUES('koza');
+
+INSERT INTO Druhy(nazev)
+    VALUES('křeček');
+
+
+INSERT INTO Nemoci(nazev)
+    VALUES('malárie');
+
+INSERT INTO Nemoci(nazev)
+    VALUES('zablešení');
+
+INSERT INTO Nemoci(nazev)
+    VALUES('rakovina');
+
+INSERT INTO Nemoci(nazev)
+    VALUES('slintavka');
+
+
+INSERT INTO Leky(nazev, typ, kontraindikace, ucinna_latka)
+    VALUES('amalar', 'antimalarika', 'alergie', 'AM2013');
+
+INSERT INTO Leky(nazev, typ, kontraindikace, ucinna_latka)
+    VALUES('blechostop2000', 'antiparazitni', 'otevřené rány', 'oxid manganičitý');
+
+INSERT INTO Leky(nazev, typ, ucinna_latka)
+    VALUES('sutlam', 'antibiotika', 'penicilin');
+
+
+INSERT INTO Urceni_leku_pro_nemoc(kod_nemoci, kod_leku)
+    VALUES((SELECT kod_nemoci FROM Nemoci WHERE nazev='slintavka'), (SELECT kod_leku FROM Leky WHERE nazev='sutlam'));
+
+INSERT INTO Urceni_leku_pro_nemoc(kod_nemoci, kod_leku)
+    VALUES((SELECT kod_nemoci FROM Nemoci WHERE nazev='malárie'), (SELECT kod_leku FROM Leky WHERE nazev='amalar'));
+
+INSERT INTO Urceni_leku_pro_nemoc(kod_nemoci, kod_leku)
+    VALUES((SELECT kod_nemoci FROM Nemoci WHERE nazev='zablešení'), (SELECT kod_leku FROM Leky WHERE nazev='blechostop2000'));
+
+INSERT INTO Urceni_leku_pro_nemoc(kod_nemoci, kod_leku)
+    VALUES((SELECT kod_nemoci FROM Nemoci WHERE nazev='zablešení'), (SELECT kod_leku FROM Leky WHERE nazev='sutlam'));
+
+
+INSERT INTO Davkovani_pro_druh(doporucene_davkovani, kod_leku, id_druhu)
+    VALUES('2x2',(SELECT kod_leku FROM Leky WHERE nazev='sutlam'), (SELECT id_druhu FROM Druhy WHERE nazev='kočka'));    
+
+INSERT INTO Davkovani_pro_druh(doporucene_davkovani, kod_leku, id_druhu)
+    VALUES('3x3',(SELECT kod_leku FROM Leky WHERE nazev='sutlam'), (SELECT id_druhu FROM Druhy WHERE nazev='koza')); 
+
+INSERT INTO Davkovani_pro_druh(doporucene_davkovani, kod_leku, id_druhu)
+    VALUES('2x denně půl tablety',(SELECT kod_leku FROM Leky WHERE nazev='sutlam'), (SELECT id_druhu FROM Druhy WHERE nazev='potkan')); 
+
+INSERT INTO Davkovani_pro_druh(doporucene_davkovani, kod_leku, id_druhu)
+    VALUES('1x mesicne 2ml',(SELECT kod_leku FROM Leky WHERE nazev='amalar'), (SELECT id_druhu FROM Druhy WHERE nazev='kočka')); 
+
+INSERT INTO Davkovani_pro_druh(doporucene_davkovani, kod_leku, id_druhu)
+    VALUES('1x mesicne 5ml',(SELECT kod_leku FROM Leky WHERE nazev='amalar'), (SELECT id_druhu FROM Druhy WHERE nazev='pes')); 
+    
+
+INSERT INTO Zvirata(jmeno, datum_narozeni, datum_posledni_prohlidky, vlastnik, druh) 
+    VALUES('Micka', DATE'1998-12-20', DATE'2019-3-19', (SELECT osobni_cislo FROM Osoby WHERE jmeno='Jan' AND prijmeni='Beran' ), (SELECT id_druhu FROM Druhy WHERE nazev='kočka' ) );
+
+INSERT INTO Zvirata(jmeno, datum_narozeni, datum_posledni_prohlidky, vlastnik, druh) 
+    VALUES('Rex', DATE'2016-12-20', DATE'2018-3-19', (SELECT osobni_cislo FROM Osoby WHERE jmeno='Kamila' AND prijmeni='Krátká' ), (SELECT id_druhu FROM Druhy WHERE nazev='pes' ) );
+
+INSERT INTO Zvirata(datum_narozeni, datum_posledni_prohlidky, vlastnik, druh) 
+    VALUES(DATE'2015-11-30', DATE'2019-3-19', (SELECT osobni_cislo FROM Osoby WHERE jmeno='Kamila' AND prijmeni='Krátká' ), (SELECT id_druhu FROM Druhy WHERE nazev='koza' ) );
+
+INSERT INTO Zvirata(jmeno, datum_narozeni, datum_posledni_prohlidky, vlastnik, druh) 
+    VALUES('Mourek', DATE'2009-10-10', DATE'2019-3-2', (SELECT osobni_cislo FROM Osoby WHERE jmeno='Jasan' AND prijmeni='Statham' ), (SELECT id_druhu FROM Druhy WHERE nazev='kočka' ) );
+
+
+INSERT INTO Lecby(diagnoza, datum_zahajeni, stav, zahajujici_osetrovatel, zvire)
+	VALUES('Vysoké teploty, pravděpodobně slintavka', DATE'2019-03-10', 'Probihajici', (SELECT rc FROM Zamestnanci Z, Osoby O WHERE Z.osobni_zaznamy=O.osobni_cislo AND O.prijmeni='Stejskal'), (SELECT cislo_zvirete FROM Zvirata WHERE jmeno='Micka'));
+
+INSERT INTO Lecby(diagnoza, datum_zahajeni, stav, zahajujici_osetrovatel, zvire)
+	VALUES('má blechy', DATE'2019-01-10', 'Ukoncena', (SELECT rc FROM Zamestnanci Z, Osoby O WHERE Z.osobni_zaznamy=O.osobni_cislo AND O.prijmeni='Stejskal'), (SELECT cislo_zvirete FROM Zvirata WHERE jmeno='Rex'));
+
+INSERT INTO Predpisy(davkovani, doba_podavani, podan_v_ordinaci, kod_lecby, predepsany_lek, vypsal)
+	VALUES('2x2 tablety denně', 'tyden', 'NE', (SELECT L.kod_lecby FROM Lecby L, Zvirata Z WHERE L.datum_zahajeni=DATE'2019-03-10' AND Z.jmeno='Micka' AND L.zvire=Z.cislo_zvirete), (SELECT kod_leku FROM Leky WHERE nazev='sutlam'), (SELECT rc FROM Zamestnanci Z, Osoby O WHERE Z.osobni_zaznamy=O.osobni_cislo AND O.prijmeni='Stejskal'));
 
